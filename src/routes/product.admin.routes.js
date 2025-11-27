949e63cc35d4715b51257f5726472805df3fad2f
federus1105 <federusrudi@gmail.com>
2025-11-28 00:38:32 +0700

    Feat ( routes ) : feature route admin create product using validation

--- src/routes/product.admin.routes.js
+++ src/routes/product.admin.routes.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import { ListProducts, CreateProductHandler } from '../handlers/product.admin.handler.js';
 import upload from '../pkg/libs/upload.js';
+import { AdminCreateProduct } from '../pkg/utils/validators/productAdminValidators.js';
+import { validate } from '../pkg/libs/validate.js';
 
 const router = express.Router();
 
@@ -10,6 +12,6 @@ router.post('/',  upload.fields([
     { name: 'image_two', maxCount: 1 },
     { name: 'image_three', maxCount: 1 },
     { name: 'image_four', maxCount: 1 },
-  ]), CreateProductHandler)
+  ]), AdminCreateProduct, validate, CreateProductHandler)
 
 export default router
