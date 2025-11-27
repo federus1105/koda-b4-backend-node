0c37ce90d0e25f98d24249b5780efeb1d950557b
federus1105 <federusrudi@gmail.com>
2025-11-27 16:34:56 +0700

    Feat ( routes ) : feature auth middleware and access token for route admin/product

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import authRoutes from "./auth.routes.js"
 import productAdminRoutes from "./product.admin.routes.js"
+import {authMiddleware, adminMiddleware } from "../middleware/auth.middleware.js";
 
 const router = Router();
 
 router.use('/auth', authRoutes);
-router.use('/admin/product', authRoutes);
+router.use('/admin/product', authMiddleware,adminMiddleware, productAdminRoutes);
 
 export default router;
