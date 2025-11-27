2b53739fc8c2d6b6362d9e6c3e09e1a1ab9c1ab3
federus1105 <federusrudi@gmail.com>
2025-11-27 23:51:32 +0700

    Feat ( index ) : feature response error from multer

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import router from './src/routes/index.js'; 
 import Initdocs from './src/pkg/libs/docs.js'
+import multer from 'multer';
 
 const app = express();
 
@@ -10,8 +11,19 @@ app.use('/uploads', express.static('src/uploads'));
 
 Initdocs(app);
 
-
 app.use('/', router);
+
+// --- ERROR MULTER ---
+app.use((err, req, res, next) => {
+  if (err instanceof multer.MulterError) {
+    return res.status(400).json({ 
+      success: false, 
+      message: err.message });
+  }
+  return res.status(500).json({ 
+    success: false, 
+    message: err.message });
+});
 
 app.get('/', (req, res)=>{
   res.json({
