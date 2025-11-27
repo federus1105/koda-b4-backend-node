08a5d03e40c8010781c92630798012f4fabbca30
federus1105 <federusrudi@gmail.com>
2025-11-27 14:34:43 +0700

    Feat ( libs ) : feature init express swagger

---
+++ src/pkg/libs/docs.js
@@ -0,0 +1,24 @@
+import expressDocsSwagger from 'express-jsdoc-swagger';
+
+function Initdocs(app){
+    const options = {
+        info: {
+            version: '1.0.0',
+            title: 'Backend Express Senja Kopi-Kiri',
+            description: 'API documentation for Backend Express Senja Kopi-kiri',
+        },
+        baseDir: process.cwd(),
+        filesPattern: [
+        './src/handlers/**.js',
+        './src/docs/*.js'
+        ],
+        swaggerUIPath: '/api-docs',
+        exposeSwaggerUI: true,
+        exposeApiDocs: false,
+        apiDocsPath: '/api-docs.json',
+        multiple: true,
+    };
+    expressDocsSwagger(app)(options);
+}
+
+export default Initdocs
