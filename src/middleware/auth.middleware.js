ebdb88387ebf9e18ea456257981baa3756e0e041
federus1105 <federusrudi@gmail.com>
2025-11-27 16:35:21 +0700

    Feat ( middleware  ) : function middleware access route only admin by token

--- src/middleware/auth.middleware.js
+++ src/middleware/auth.middleware.js
@@ -1,13 +1,15 @@
+import jwt from "jsonwebtoken";
+
 /**
  * 
  * @param {Express("Express").Request} req 
  * @param {Express("Express").Response} res 
- * @param {import( Express").NextFunction} next 
+ * @param {import("express").NextFunction} next
  */
-function authMiddleware(req, res, next) {
+export function authMiddleware(req, res, next) {
 const bearer = req.headers?.authorization ?? '';
-const prefix = 'Beraer ';
-if(!bearer.startWIth(prefix)) {
+const prefix = 'Bearer ';
+if(!bearer.startsWith(prefix)) {
     res.status(401).json({
         success :false,
         message: "Unauthorized Access Missing token"
@@ -16,8 +18,8 @@ if(!bearer.startWIth(prefix)) {
 }
 const token = bearer.substring(prefix.length);
 try {
-const payload = jwt.veify(token, process.env.JWT_SECRET)
-req.jwtPayload =payload;
+const payload = jwt.verify(token, process.env.JWT_SECRET)
+req.jwtPayload = payload;
 next()
 } catch (error) {
     res.status(401).json({
@@ -27,4 +29,15 @@ next()
 }
 }
 
-export default authMiddleware
+export function adminMiddleware(req, res, next) {
+    const userRole = req.jwtPayload?.role;
+
+    if (userRole !== 'admin') {
+        return res.status(403).json({
+            success: false,
+            message: "only admin can access this resource"
+        });
+    }
+
+    next();
+}
