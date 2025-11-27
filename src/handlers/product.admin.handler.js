344f16720b41d84f2739cb380e14c4431ed4df8a
federus1105 <federusrudi@gmail.com>
2025-11-27 23:53:32 +0700

    Feat ( handlers ) : feature handler create product

--- src/handlers/product.admin.handler.js
+++ src/handlers/product.admin.handler.js
@@ -1,4 +1,8 @@
-import { getAllProducts } from "../models/product.admin.models.js";
+import { CreateProduct, getAllProducts } from "../models/product.admin.models.js";
+import { normalizeInput } from "../pkg/utils/common.js";
+import { getPrisma } from '../pkg/libs/prisma.js';
+import path from 'path'
+const prisma = getPrisma();
 
 export async function ListProducts(req, res) {
     try {
@@ -42,4 +46,78 @@ export async function ListProducts(req, res) {
             error: error.message,
         });
     }
+}
+
+
+export async function CreateProductHandler(req, res) {
+  try {
+    const size = normalizeInput(req.body.size);
+    const variant = normalizeInput(req.body.variant);
+    const category = normalizeInput(req.body.category);
+
+    const image = {
+      photosOne: req.files.image_one?.[0]?.path ? path.basename(req.files.image_one[0].path) : null,
+      photosTwo: req.files.image_two?.[0]?.path || null,
+      photosThree: req.files.image_three?.[0]?.path || null,
+      photosFour: req.files.image_four?.[0]?.path || null,
+    };
+
+    const product = await CreateProduct({
+      name: req.body.name,
+      description: req.body.description,
+      rating: req.body.rating,
+      priceOriginal: req.body.price,
+      stock: req.body.stock,
+      image,
+      size,
+      variant,
+      category,
+    }); 
+
+    //  --- GET DATA FOR RESPONSE ---
+    const [images, sizes, variants, categories] = await Promise.all([
+      prisma.productImages.findUnique({
+        where: { id: product.id_product_images },
+      }),
+      prisma.sizeProduct.findMany({
+        where: { id_product: product.id },
+        select: { id_size: true },
+      }),
+      prisma.variantProduct.findMany({
+        where: { id_product: product.id },
+        select: { id_variant: true },
+      }),
+      prisma.productCategories.findMany({
+        where: { id_product: product.id },
+        select: { id_categories: true },
+      }),
+    ]);
+
+    // --- RESPONSE ---
+    const response = {
+      id: product.id,
+      name: product.name,
+      id_image: product.id_product_images,
+      images: images,
+      price: product.priceOriginal,
+      rating: product.rating,
+      description: product.description,
+      stock: product.stock,
+      size: sizes.map(s => s.id_size),
+      variant: variants.map(v => v.id_variant),
+      category: categories.map(c => c.id_categories),
+    };
+
+    res.status(201).json({
+      success: true,
+      message: "Product created successfully",
+      results: response,
+    });
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: "Internal server error",
+      error: error.message,
+    });
+  }
 }
