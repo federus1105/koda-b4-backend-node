d5d6f829e9b632195a0ca8e076c3d89a7356efca
federus1105 <federusrudi@gmail.com>
2025-11-27 16:35:55 +0700

    Feat ( handlers  ):  fearure pagination handler for get list product

--- src/handlers/product.admin.handler.js
+++ src/handlers/product.admin.handler.js
@@ -1,13 +1,38 @@
-import { getAllProducts } from "../models/product.admin.models";
+import { getAllProducts } from "../models/product.admin.models.js";
 
 export async function ListProducts(req, res) {
     try {
         const filters = { name: req.query.name || '' };
-        const products = await getAllProducts(filters);
+
+        // --- PAGINATION ---
+        const page = parseInt(req.query.page) || 1;
+        const limit = 10;
+        const skip = (page - 1) * limit;
+
+        // --- GET DATA ---
+        const products = await getAllProducts({
+            ...filters,
+            skip,
+            take: limit,
+        });
+
+        const total = await getAllProducts({ name: filters.name, countOnly: true });
+
+        if (products.length === 0) {
+            res.status(404).json({
+                success: false,
+                message: "Product not found"
+            })
+        return
+        }
 
         res.status(200).json({
+            page,
+            limit,
+            total,
+            totalPages: Math.ceil(total / limit),
             success: true,
-            message: "List of products",
+            message: "Get list data successfully",
             results: products,
         });
     } catch (error) {
