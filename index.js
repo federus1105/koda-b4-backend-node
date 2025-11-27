16285046a38f6e4e44eb8a66b38efdbea4054ea8
federus1105 <federusrudi@gmail.com>
2025-11-27 12:02:25 +0700

    Feat ( index ) : feature main root for this project

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from 'express';
+import router from './src/routes/index.js'; 
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+app.use('/uploads', express.static('src/uploads'));
+
+Initdocs(app);
+
+app.use('/', router);
+
+app.get('/', (req, res)=>{
+  res.json({
+    message: 'Backend is running well',
+  })
+})
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
