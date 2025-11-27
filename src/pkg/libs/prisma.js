483efb5f522743aeb9ca3786c49a8d5e51565c14
federus1105 <federusrudi@gmail.com>
2025-11-27 14:27:51 +0700

    Fix ( libs ) : path for prisma client import

--- src/pkg/libs/prisma.js
+++ src/pkg/libs/prisma.js
@@ -1,7 +1,7 @@
 import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import "dotenv/config";
 import { env } from "node:process";
-import { PrismaClient } from "../../generated/prisma/index.js";
+import { PrismaClient } from "../../../generated/prisma/index.js";
 
 const connectionString = `${env.DATABASE_URL}`;
 
