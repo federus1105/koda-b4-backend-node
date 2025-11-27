4548af0bfe7dce833b6706864878a7925801900d
federus1105 <federusrudi@gmail.com>
2025-11-28 00:02:05 +0700

    Feat ( routes ) : feature route create product using upload file

--- src/routes/product.admin.routes.js
+++ src/routes/product.admin.routes.js
@@ -1,8 +1,15 @@
 import express from 'express';
-import { ListProducts } from '../handlers/product.admin.handler.js';
+import { ListProducts, CreateProductHandler } from '../handlers/product.admin.handler.js';
+import upload from '../pkg/libs/upload.js';
 
 const router = express.Router();
 
 router.get('/', ListProducts)
+router.post('/',  upload.fields([
+    { name: 'image_one', maxCount: 1 },
+    { name: 'image_two', maxCount: 1 },
+    { name: 'image_three', maxCount: 1 },
+    { name: 'image_four', maxCount: 1 },
+  ]), CreateProductHandler)
 
 export default router
