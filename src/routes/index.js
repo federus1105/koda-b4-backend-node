9d3e73d0844d0a5fe2a717a05a9899743aea8080
federus1105 <federusrudi@gmail.com>
2025-11-29 05:33:07 +0700

    Feat ( routes ) : feature route for product

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,11 +3,13 @@ import authRoutes from "./auth.routes.js"
 import productAdminRoutes from "./product.admin.routes.js"
 import {authMiddleware, adminMiddleware } from "../middleware/auth.middleware.js";
 import profileRoutes from "./profile.routes.js"
+import productRoutes from "./product.routes.js"
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/admin/product', authMiddleware, adminMiddleware, productAdminRoutes);
 router.use('/profile', authMiddleware, profileRoutes);
+router.use('/product', productRoutes);
 
 export default router;
