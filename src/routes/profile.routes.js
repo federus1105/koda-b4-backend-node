8a16601ae15979d7c2e4c4debc452ff8367e9341
federus1105 <federusrudi@gmail.com>
2025-11-29 04:34:43 +0700

    Fix : change the placement for the auth middleware to the index route in the profile

--- src/routes/profile.routes.js
+++ src/routes/profile.routes.js
@@ -1,13 +1,12 @@
 import express from 'express';
 import { updateProfileHandler } from '../handlers/profile.handlers.js';
-import { authMiddleware } from '../middleware/auth.middleware.js';
 import upload from '../pkg/libs/upload.js';
 import { updateProfileValidator } from '../pkg/utils/validators/profileValidators.js';
 import { validate } from '../pkg/libs/validate.js';
 
 const router = express.Router();
 
-router.patch('/', authMiddleware, upload.single("photos"), updateProfileValidator, validate, updateProfileHandler )
+router.patch('/', upload.single("photos"), updateProfileValidator, validate, updateProfileHandler )
 
 
 export default router
