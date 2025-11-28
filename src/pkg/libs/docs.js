f8998107ec51ba6a1b3da73550d2a2b53d13bbe7
federus1105 <federusrudi@gmail.com>
2025-11-28 14:41:53 +0700

    Feat ( libs ) : feature security using bearer jwt auth

--- src/pkg/libs/docs.js
+++ src/pkg/libs/docs.js
@@ -1,24 +1,31 @@
 import expressDocsSwagger from 'express-jsdoc-swagger';
 
-function Initdocs(app){
-    const options = {
-        info: {
-            version: '1.0.0',
-            title: 'Backend Express Senja Kopi-Kiri',
-            description: 'API documentation for Backend Express Senja Kopi-kiri',
-        },
-        baseDir: process.cwd(),
-        filesPattern: [
-        './src/handlers/**.js',
-        './src/docs/*.js'
-        ],
-        swaggerUIPath: '/api-docs',
-        exposeSwaggerUI: true,
-        exposeApiDocs: false,
-        apiDocsPath: '/api-docs.json',
-        multiple: true,
-    };
-    expressDocsSwagger(app)(options);
+function Initdocs(app) {
+  const options = {
+    info: {
+      version: '1.0.0',
+      title: 'Backend Express Senja Kopi-Kiri',
+      description: 'API documentation for Backend Express Senja Kopi-kiri',
+    },
+    baseDir: process.cwd(),
+    filesPattern: [
+      './src/handlers/**.js',
+      './src/docs/**.js'
+    ],
+    swaggerUIPath: '/api-docs',
+    exposeSwaggerUI: true,
+    exposeApiDocs: false,
+    apiDocsPath: '/api-docs.json',
+    multiple: true,
+    security: {
+      bearerAuth: {
+        type: "http",
+        scheme: "bearer",
+        bearerFormat: "JWT"
+      },
+  },
+  };
+  expressDocsSwagger(app)(options);
 }
 
 export default Initdocs
