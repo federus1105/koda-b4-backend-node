28fbea72e5ab86c9d99f86a8a863ae47c07bf64a
federus1105 <federusrudi@gmail.com>
2025-11-29 04:36:34 +0700

    Feat ( routes ) : feature route for profile using auth middleware

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,12 @@ import { Router } from "express";
 import authRoutes from "./auth.routes.js"
 import productAdminRoutes from "./product.admin.routes.js"
 import {authMiddleware, adminMiddleware } from "../middleware/auth.middleware.js";
+import profileRoutes from "./profile.routes.js"
 
 const router = Router();
 
 router.use('/auth', authRoutes);
 router.use('/admin/product', authMiddleware, adminMiddleware, productAdminRoutes);
+router.use('/profile', authMiddleware, profileRoutes);
 
 export default router;
