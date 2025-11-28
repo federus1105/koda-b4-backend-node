cdc97e5c9dac7ad6cf2a82d1ffca0d0ece3e4472
federus1105 <federusrudi@gmail.com>
2025-11-28 17:34:14 +0700

    Feat ( libs ) : feature connected using prisma pg

--- src/pkg/libs/prisma.js
+++ src/pkg/libs/prisma.js
@@ -3,10 +3,10 @@ import { env } from "node:process";
 import { PrismaClient } from "../../../generated/prisma/index.js";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const connectionString = `${env.DATABASE_URL}`;
+const connectionString = `${process.env.DATABASE_URL}`
 
-const adapter = new PrismaPg({ url: connectionString });
-const prisma = new PrismaClient({ adapter });
+const adapter = new PrismaPg({ connectionString })
+const prisma = new PrismaClient({ adapter })
 
 export { prisma };
 
