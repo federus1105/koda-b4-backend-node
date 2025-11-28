e35f9716b13fac8aadd9bce2846438929743c626
federus1105 <federusrudi@gmail.com>
2025-11-28 15:42:16 +0700

    Fix : using prisma pg for adapter

--- src/pkg/libs/prisma.js
+++ src/pkg/libs/prisma.js
@@ -1,11 +1,11 @@
-import { PrismaBetterSqlite3 } from "@prisma/adapter-better-sqlite3";
 import "dotenv/config";
 import { env } from "node:process";
 import { PrismaClient } from "../../../generated/prisma/index.js";
+import { PrismaPg } from "@prisma/adapter-pg";
 
 const connectionString = `${env.DATABASE_URL}`;
 
-const adapter = new PrismaBetterSqlite3({ url: connectionString });
+const adapter = new PrismaPg({ url: connectionString });
 const prisma = new PrismaClient({ adapter });
 
 export { prisma };
