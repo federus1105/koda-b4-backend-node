4c859db4b359d77677de2ecc3559350e6c4f4d55
federus1105 <federusrudi@gmail.com>
2025-11-29 22:05:32 +0700

    Feat ( utils ) : feature express validators for order feature

--- src/pkg/utils/validators/ordersValidators.js
+++ src/pkg/utils/validators/ordersValidators.js
@@ -9,3 +9,31 @@ export const CreateCartValidator = [
     .exists().withMessage("Quantity is required")
     .isInt({ gt: 0 }).withMessage("Quantity must be greater than 0"),
 ];
+
+export const orderValidator = [
+  body("fullname")
+    .optional({ nullable: true })
+    .isString().withMessage("Fullname must be a string")
+    .isLength({ min: 3, max: 30 }).withMessage("Fullname must be between 3–30 characters"),
+
+  body("phone")
+    .optional({ nullable: true })
+    .isMobilePhone("id-ID").withMessage("Invalid phone number"),
+
+  body("address")
+    .optional({ nullable: true })
+    .isString().withMessage("Address must be a string")
+    .isLength({ min: 5, max: 50 }).withMessage("Address must be between 5–50 characters"),
+
+  body("email")
+    .optional({ nullable: true })
+    .isEmail().withMessage("Invalid email format"),
+
+  body("id_paymentMethod")
+    .notEmpty().withMessage("Payment method is required")
+    .isInt({ min: 1 }).withMessage("Payment method must be a valid ID"),
+
+  body("id_delivery")
+    .notEmpty().withMessage("Delivery method is required")
+    .isInt({ min: 1 }).withMessage("Delivery method must be a valid ID"),
+];
