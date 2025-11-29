8b1530bb5f53d03fa28e011f9054102e262aa1f8
federus1105 <federusrudi@gmail.com>
2025-11-29 15:37:29 +0700

    Feat ( route ) : feature route for filter product

--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { FavoriteProductsHandler } from '../handlers/product.handlers.js';
+import { FavoriteProductsHandler, FilterProductHandler } from '../handlers/product.handlers.js';
 
 const router = express.Router();
 
 router.get('/favorite-product', FavoriteProductsHandler)
+router.get('/', FilterProductHandler)
 
 
 export default router
