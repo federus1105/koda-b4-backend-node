201f8607ba9ee427ba115ad1f81303045044651f
federus1105 <federusrudi@gmail.com>
2025-11-29 19:56:33 +0700

    Feat ( routes ) : init feature get cart for user concerned

--- src/routes/order.routes.js
+++ src/routes/order.routes.js
@@ -1,11 +1,12 @@
 import express from 'express';
-import { CreateCartHandler } from '../handlers/orders.handlers.js';
+import { CreateCartHandler, GetCartHandler } from '../handlers/orders.handlers.js';
 import { CreateCartValidator } from '../pkg/utils/validators/ordersValidators.js';
 import { validate } from '../pkg/libs/validate.js';
 
 const router = express.Router();
 
 router.post('/cart', CreateCartValidator, validate, CreateCartHandler)
+router.get('/cart', GetCartHandler)
 
 
 export default router
