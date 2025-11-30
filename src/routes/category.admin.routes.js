00d956830d1f0b98c8931215666d4452c540ec62
federus1105 <federusrudi@gmail.com>
2025-11-30 18:09:23 +0700

    Feat ( routes ) : feature validator for create category

--- src/routes/category.admin.routes.js
+++ src/routes/category.admin.routes.js
@@ -1,10 +1,12 @@
 import express from 'express';
 import { CreateCategoryHandler, ListCategoryHandler } from '../handlers/category.admin.handlers.js';
+import { createCategoryValidator } from '../pkg/utils/validators/category.admin.validators.js';
+import { validate } from '../pkg/libs/validate.js';
 
 const router = express.Router();
 
 router.get('/', ListCategoryHandler)
-router.post('/', CreateCategoryHandler)
+router.post('/', createCategoryValidator, validate, CreateCategoryHandler)
 
 
 export default router
