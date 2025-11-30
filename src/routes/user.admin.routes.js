b845fd86dd16b99ca72ec98571ee17c51892e2c8
federus1105 <federusrudi@gmail.com>
2025-11-30 17:03:20 +0700

    Feat ( routes ) : init routes feature update user with using validators custom

--- src/routes/user.admin.routes.js
+++ src/routes/user.admin.routes.js
@@ -1,13 +1,14 @@
 import express from 'express';
-import { CreateUserHandler, ListUserHandler } from '../handlers/user.admin.handlers.js';
+import { CreateUserHandler, ListUserHandler, UpdateUserHandler } from '../handlers/user.admin.handlers.js';
 import upload from '../pkg/libs/upload.js';
-import { CreateUserValidator } from '../pkg/utils/validators/user.admin.validators.js';
+import { CreateUserValidator, UpdateUserValidator } from '../pkg/utils/validators/user.admin.validators.js';
 import { validate } from '../pkg/libs/validate.js';
 
 const router = express.Router();
 
 router.get('/', ListUserHandler)
 router.post('/', upload.single("photos"), CreateUserValidator, validate , CreateUserHandler)
+router.patch('/:id', upload.single("photos"), UpdateUserValidator, validate, UpdateUserHandler)
 
 
 export default router
