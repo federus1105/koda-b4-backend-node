47ec2d9df2ee122fe5e8bc2fad279df0b335c7cf
federus1105 <federusrudi@gmail.com>
2025-11-30 17:02:36 +0700

    Feat ( utils ) : feature custome express validator for update user feature

--- src/pkg/utils/validators/user.admin.validators.js
+++ src/pkg/utils/validators/user.admin.validators.js
@@ -32,3 +32,27 @@ export const CreateUserValidator = [
     .notEmpty().withMessage("Role is required")
     .isIn(["user", "admin"]).withMessage("Role must be either 'user' or 'admin'"),
 ];
+
+export const UpdateUserValidator = [
+  body("fullname")
+    .optional({ nullable: true })
+    .isString().withMessage("Fullname must be a string")
+    .isLength({ min: 3, max: 30 }).withMessage("Fullname must be between 3–30 characters"),
+
+  body("phone")
+    .optional({ nullable: true })
+    .isMobilePhone("id-ID").withMessage("Invalid phone number"),
+
+  body("address")
+    .optional({ nullable: true })
+    .isString().withMessage("Address must be a string")
+    .isLength({ min: 5, max: 50 }).withMessage("Address must be between 5–50 characters"),
+
+  body("email")
+    .optional({ nullable: true })
+    .isEmail().withMessage("Invalid email format"),
+
+  body("role")
+    .optional({ nullable: true })
+    .isIn(["user", "admin"]).withMessage("Role must be either 'user' or 'admin'"),
+];
