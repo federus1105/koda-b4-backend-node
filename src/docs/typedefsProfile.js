7cab991566d7632d0c9a3c49753eebaf7d0dc3ab
federus1105 <federusrudi@gmail.com>
2025-11-30 14:07:27 +0700

    Feat ( docs ) : feature typedef profile for request body update profile

---
+++ src/docs/typedefsProfile.js
@@ -0,0 +1,8 @@
+/**
+ * @typedef {object} UpdateProfile
+ * @property {string} fullname - fullname
+ * @property {string} phone - phone
+ * @property {number} email - email
+ * @property {number} address - addres
+ * @property {string} photos -  photos - binary
+ */
