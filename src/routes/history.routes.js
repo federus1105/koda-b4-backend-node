69c20a55e4c8442df9c192cd0467f58899761c0d
federus1105 <federusrudi@gmail.com>
2025-11-30 11:04:57 +0700

    Feat ( routes ) : inisialization routes for detail history

--- src/routes/history.routes.js
+++ src/routes/history.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { HistoryHandler } from '../handlers/history.handlers.js';
+import { DetailHistoryHandler, HistoryHandler } from '../handlers/history.handlers.js';
 
 const router = express.Router();
 
 router.get('/', HistoryHandler)
+router.get('/:id', DetailHistoryHandler)
 
 
 export default router
