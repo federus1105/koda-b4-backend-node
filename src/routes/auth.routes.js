d7fbb0c2f1125a0437b111de1b15794cbcff88c0
federus1105 <federusrudi@gmail.com>
2025-11-30 21:27:15 +0700

    Feat ( routes ) : init feature forgor and reset password

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,12 +1,14 @@
 import express from 'express';
-import { Register,Login } from '../handlers/auth.handlers.js';
+import { Register, Login, ForgotPassword, ResetPassword } from '../handlers/auth.handlers.js';
 import { validate } from '../pkg/libs/validate.js';
-import { loginValidation, registerValidation } from '../pkg/utils/validators/authValidators.js';
+import { ForgotPasswordValidator, loginValidation, registerValidation, ResetPasswordValidator } from '../pkg/utils/validators/authValidators.js';
 
 const router = express.Router();
 
 router.post('/register', registerValidation, validate, Register)
 router.post('/login', loginValidation, validate, Login )
+router.post('/forgot-password', ForgotPasswordValidator, validate, ForgotPassword )
+router.post('/reset-password', ResetPasswordValidator, validate, ResetPassword )
 
 
 export default router
