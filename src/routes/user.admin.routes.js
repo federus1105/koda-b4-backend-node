64efd46f43db03a5a07e8bc3c5688a7bf136d0f3
federus1105 <federusrudi@gmail.com>
2025-11-30 16:37:34 +0700

    Feat ( routes ) : init routes for feature create user

--- src/routes/user.admin.routes.js
+++ src/routes/user.admin.routes.js
@@ -1,9 +1,13 @@
 import express from 'express';
-import { ListUserHandler } from '../handlers/user.admin.handlers.js';
+import { CreateUserHandler, ListUserHandler } from '../handlers/user.admin.handlers.js';
+import upload from '../pkg/libs/upload.js';
+import { CreateUserValidator } from '../pkg/utils/validators/user.admin.validators.js';
+import { validate } from '../pkg/libs/validate.js';
 
 const router = express.Router();
 
 router.get('/', ListUserHandler)
+router.post('/', upload.single("photos"), CreateUserValidator, validate , CreateUserHandler)
 
 
 export default router
